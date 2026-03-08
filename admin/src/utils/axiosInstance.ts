a9424e209a0f91aa316847515cce1de60553b470
deepmoina2005 <deepmoinaboruah7@gmail.com>
2026-03-08 23:51:05 +0530

    commit

--- admin/src/utils/axiosInstance.ts
+++ admin/src/utils/axiosInstance.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://my-site-zcyn.vercel.app/api",
 });
 
 axiosInstance.interceptors.request.use((config) => {
